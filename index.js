730a313e6e054221368a8beb34c3626591abc9f3
Gytis Žaltauskas <gytis.zaltauskas@gmail.com>
2021-10-06 19:04:51 +0300

    Trikampio klasės implementacija

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-var vardas = "Jurgis";
-var x = 1;
-var y = 1;
-function suma(skaicius1, skaicius2) {
-    return skaicius1 + skaicius2;
-}
-1 + "1";
-1 + 1;
-var z = x + y;
-var vardai = ["Jonas", "Birutė", "Barbora", "Elena"];
-vardai.push("Gediminas");
-function pasisveikinti(pasisveikinimas, vardas) {
-    console.log(pasisveikinimas + ", " + vardas);
-}
-function pasisveikintiSuZmonemis(vardai) {
-    var iteratorius = function (vardas) {
-        pasisveikinti("Labas", vardas);
-    };
-    vardai.forEach(iteratorius);
-}
-pasisveikintiSuZmonemis(vardai);
-var Gyvenviete = (function () {
-    function Gyvenviete(pavadinimas, gyventojuSkaicius, plotas) {
-        console.log("Konstruktorius iškviestas");
-        this.pavadinimas = pavadinimas;
-        this.gyventojuSkaicius = gyventojuSkaicius;
-        this.plotas = plotas;
+var Trikampis = (function () {
+    function Trikampis(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
-    Gyvenviete.prototype.spausdintiDuomenis = function () {
-        console.log("Miesto pavadinimas: " + this.pavadinimas);
-        console.log("Miesto gyventoju skai\u010Dius: " + this.gyventojuSkaicius);
-        console.log("Miesto plotas: " + this.plotas + " km2");
-        console.log("Miesto tankumas: " + this.gyventojuTankumas().toFixed(1) + " gyv./km2");
-        console.log("------------");
+    Trikampis.prototype.spaudintiDuomenis = function () {
+        console.log("Krastine a: " + this.a);
+        console.log("Krastine b: " + this.b);
+        console.log("Krastine b: " + this.c);
+        console.log("---------");
     };
-    Gyvenviete.prototype.gyventojuTankumas = function () {
-        return this.gyventojuSkaicius / this.plotas;
-    };
-    return Gyvenviete;
+    return Trikampis;
 }());
-var gyvenviete1 = new Gyvenviete("Vilnius", 587581, 401);
-var gyvenviete2 = new Gyvenviete("Klaipėda", 149157, 110);
-gyvenviete1.spausdintiDuomenis();
-gyvenviete2.spausdintiDuomenis();
-console.log(gyvenviete1.pavadinimas);
+var trikampis1 = new Trikampis(2, 3, 4);
+var trikampis2 = new Trikampis(6, 3, 8);
+trikampis1.spaudintiDuomenis();
+trikampis2.spaudintiDuomenis();
--- index.ts
+++ index.ts
@@ -11,9 +11,33 @@
  *
  * 5. Aprašykite metodą "spausdintiDuomenis()", kuris atspausdina
  * visus trikampio duomenis (kraštines) į konsolę.
+ *
+ * 6. Papildykite klasę metodu, kuris apskaičiuoja ir grąžina
+ * trikampio perimetrą. Spausdinant trikampio duomenis
+ * atspausdinkite ir jo perimetrą.
  */
 
-class Triangle {
+class Trikampis {
     public a: number;
+    public b: number;
+    public c: number;
 
+    public constructor(a: number, b: number, c: number) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public spaudintiDuomenis(): void {
+        console.log(`Krastine a: ${this.a}`);
+        console.log(`Krastine b: ${this.b}`);
+        console.log(`Krastine b: ${this.c}`);
+        console.log("---------");
+    }
 }
+
+const trikampis1 = new Trikampis(2, 3, 4);
+const trikampis2 = new Trikampis(6, 3, 8);
+
+trikampis1.spaudintiDuomenis();
+trikampis2.spaudintiDuomenis();
